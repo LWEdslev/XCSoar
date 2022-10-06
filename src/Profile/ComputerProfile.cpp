bb48cb6e1a770edfafeb97d7a7da2f10d5a4521b
August2111 <Info@FlapsOnline.de>
2022-10-06 13:48:52 +0200

    W2: Profile/ComputerProfile.cpp change pilot_id to pilot.id

--- src/Profile/ComputerProfile.cpp
+++ src/Profile/ComputerProfile.cpp
@@ -92,7 +92,7 @@ Profile::Load(const ProfileMap &map, WeGlideSettings &settings)
 {
   map.Get(ProfileKeys::WeGlideEnabled, settings.enabled);
   map.Get(ProfileKeys::WeGlideAutomaticUpload, settings.automatic_upload);
-  map.Get(ProfileKeys::WeGlidePilotID, settings.pilot_id);
+  map.Get(ProfileKeys::WeGlidePilotID, settings.pilot.id);
 
   const char *date = map.Get(ProfileKeys::WeGlidePilotBirthDate);
   if (date != nullptr) {
