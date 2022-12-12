August2111/OpenSoar

MSVC-all Math/Constants.hpp WIN32: include windows constants before math.h

src/Math/Constants.hpp
@@ -24,6 +24,10 @@ Copyright_License {
 #ifndef XCSOAR_MATH_CONSTANTS_HPP
 #define XCSOAR_MATH_CONSTANTS_HPP
 
+#ifdef _WIN32
+# include <corecrt_math_defines.h>
+#endif
+
 #include <math.h>
 
 #ifndef M_PI

LWEdslev/XCSoar

src/Math/Constants.hpp
@@ -22,6 +22,11 @@ Copyright_License {
 */
 
 #pragma once
+
+#ifdef __MSVC__
+# include <corecrt_math_defines.h>
+#endif
+
 #include <math.h>
 
 #ifndef M_PI